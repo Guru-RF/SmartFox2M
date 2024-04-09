407573423b630b698103dfdd7f229c1b10dcd152
Joeri van Dooren <joeri@weepee.org>
2024-04-10 01:04:36 +0200

    Update code.c

--- arduino/code.c
+++ arduino/code.c
@@ -1,6 +1,10 @@
 #include "si5351.h"
 #include "Wire.h"
 
+//bool setSDA(26);
+//bool setSCL(27);
+
+
 Si5351 si5351;
 
 //#define PLLB_FREQ    87250000000ULL
@@ -9,40 +13,47 @@ Si5351 si5351;
 
 void setup()
 {
-  delay(2000);
-  Serial.println("Enable I2C bus");
-  Wire.begin(1);    // I2C bus address = 1
-  delay(3000);
-  si5351.reset();
-  Serial.println("Reset SI5351");
+
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+
+
+  Wire1.setSDA(26);
+  Wire1.setSCL(27);
+
+
+  //Serial.println("Enable I2C bus");
+  //Wire1.begin();    // I2C bus address = 1
+  bool i2c_found;
+
+  // Start serial and initialize the Si5351
+  i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
+  if(!i2c_found)
+  {
+    Serial.println("Device not found on I2C bus!");
+  }
+  //Serial.println("Reset SI5351");
+  //si5351.reset();
+  //delay(100);
+  //si5351.pll_reset(SI5351_PLLB);
+  //delay(100);
+  //si5351.reset();
+  //delay(100);
   si5351.init(SI5351_CRYSTAL_LOAD_8PF,0,0);
   si5351.set_vcxo(PLLB_FREQ, 61); //0->232 (deviation)
   si5351.set_ms_source(SI5351_CLK0, SI5351_PLLB);
   si5351.set_freq_manual(14545800000ULL, PLLB_FREQ, SI5351_CLK0);
   si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_2MA);
   
-  si5351.update_status();
-  delay(500);
+  //si5351.update_status();
+  //delay(100);
 
 }
 
 void loop()
 {
-
-  si5351.update_status();
-  Serial.print("  SYS_INIT: ");
-  Serial.print(si5351.dev_status.SYS_INIT);
-  Serial.print("  LOL_A: ");
-  Serial.print(si5351.dev_status.LOL_A);
-  Serial.print("  LOL_B: ");
-  Serial.print(si5351.dev_status.LOL_B);
-  Serial.print("  LOS: ");
-  Serial.print(si5351.dev_status.LOS);
-  Serial.print("  REVID: ");
-  Serial.println(si5351.dev_status.REVID);
-
   si5351.output_enable(SI5351_CLK0, 1);
-  delay(100);
+  //delay(100);
   for (int i=0; i<5; i++)  {
     Serial.println("Ring");
     tone(15, 425, 1000);
@@ -66,6 +77,8 @@ void loop()
     delay(167);
   }
   si5351.output_enable(SI5351_CLK0, 0);
+  
+  digitalWrite(11, LOW);
 
   delay(20000);
   
